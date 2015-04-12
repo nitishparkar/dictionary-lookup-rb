59e5e2a80ff1d65b1a3010a9de7c44df7e70b6ce
Nitish Parkar <developer.nitish@gmail.com>
2015-04-12 12:31:49 +0530

    updated tests

--- test/test_dictionary_lookup.rb
+++ test/test_dictionary_lookup.rb
@@ -2,8 +2,14 @@ require 'minitest/autorun'
 require 'dictionary_lookup'
 
 class DictionaryLookupTest < Minitest::Test
-  def test_hi
-    assert_equal "Hello, world!",
-      DictionaryLookup.hi
+  def test_valid_input
+    results = DictionaryLookup::Base.define("hello")
+    assert_instance_of Array, results
+    refute_equal 0, results.length
+  end
+
+  def test_empty_string_input
+    assert_equal [],
+      DictionaryLookup::Base.define("")
   end
 end
